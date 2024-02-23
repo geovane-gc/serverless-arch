a272aeb76728c83a0c33b332daf9a6cdea65b679
Geovane Gallotti Cazal <geovanegallotticazal@hotmail.com>
2024-02-23 13:31:38 -0300

    remove unnecessary file

--- src/core/types/pagination.types.ts
+++
@@ -1,9 +0,0 @@
-export type Pagination<T = object> = {
-  data: T[];
-  paginationToken: string | null;
-};
-
-export type PaginationInput = {
-  paginationToken?: string | null;
-  limit?: number | null;
-};
